6ee40ed36a555f11d877ca2b375872993ece4dc7
brixJordan <brixcarmelo@gmail.com>
2024-11-22 13:33:23 +0800

    updated portfolio website

--- mail.php
+++ mail.php
@@ -1,8 +1,30 @@
 <?php
-$to = "brixcarmelo@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: brixcarmelo@gmail.com" . "\r\n";
-
-mail($to,$subject,$txt,$headers);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Email details
+    $to = "brixcarmelo@gmail.com";
+    $subject = "New Contact Message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    
+    // Email body
+    $body = "
+        <h2>New Contact Message</h2>
+        <p><strong>Name:</strong> $name</p>
+        <p><strong>Email:</strong> $email</p>
+        <p><strong>Message:</strong></p>
+        <p>$message</p>
+    ";
+    
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send your message. Please try again.";
+    }
+}
 ?>
